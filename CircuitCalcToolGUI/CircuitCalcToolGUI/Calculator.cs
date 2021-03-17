66c354b9cea63a30f5de7a19c399f7c4ac367c69
Emanuel Ableitner <ableit190030@sr.htlweiz.at>
2021-03-17 21:45:28 +0100

    Added basic functions

---
+++ CircuitCalcToolGUI/CircuitCalcToolGUI/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircuitCalcToolGUI
+{
+    class Calculator
+    {
+        private float U = 0f, I = 0f, Ri = 0f;
+
+        public void setU(float U)
+        {
+            this.U = U;
+        }
+        public void setI(float I)
+        {
+            this.I = I;
+        }
+        public void setRi(float Ri)
+        {
+            this.Ri = Ri;
+        }
+
+        public float getResult()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CircuitCalcToolGUI/CircuitCalcToolGUI/GUI.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircuitCalcToolGUI
+{
+    struct Position
+    {
+        public Position(int X, int Y)
+        {
+            x = X;
+            y = Y;
+        }
+
+        public int x;
+        public int y;
+    }
+
+    class GUI
+    {
+        private Position cursorPos = new Position(0, 0);
+
+        readonly string top;
+        readonly string secTop;
+        readonly string bottom;
+
+        public GUI()
+        {
+            Console.SetWindowSize(60, 30 + 1); // +1 for extra line below last line
+
+            top = ".----------------------------------------------------------.";
+            secTop = "|                                                          |";
+            bottom = "'----------------------------------------------------------'";
+
+            InitDraw();
+
+            MoveCursor(new Position(1, 1));
+
+            GetUserInput();
+        }
+
+        public void MoveCursor(Position pos)
+        {
+            Console.SetCursorPosition(pos.x, pos.y);
+            cursorPos = pos;
+            //Console.Write(pos.x); Console.Write(pos.y);
+        }
+
+        private void InitDraw()
+        {
+            Console.WriteLine(top);
+            for(int c = 0; c < 28; c++)
+            {
+                Console.WriteLine(secTop);
+            }
+            Console.WriteLine(bottom);
+        }
+
+        private void GetUserInput()
+        {
+            bool stop = false;
+            
+            while (!stop)
+            {
+                ConsoleKeyInfo key = Console.ReadKey();
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.Tab:
+                        stop = true;
+                        break;
+
+                    case ConsoleKey.Enter:
+                        throw new NotImplementedException();
+                        break;
+
+                    case ConsoleKey.W:
+                        Position oldPos = cursorPos;
+                        MoveCursor(new Position(oldPos.x, oldPos.y - 1));
+                        //Console.Write("T");
+                        break;
+
+                    case ConsoleKey.D:
+                        oldPos = cursorPos;
+                        MoveCursor(new Position(oldPos.x + 1, oldPos.y));
+                        break;
+
+                    case ConsoleKey.A:
+                        oldPos = cursorPos;
+                        MoveCursor(new Position(oldPos.x - 1, oldPos.y));
+                        break;
+
+                    default:
+                        Console.WriteLine(key.KeyChar);
+                        break;
+                }
+            }
+        }
+    }
+}
